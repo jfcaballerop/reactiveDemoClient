1a3b5273e3440f4a5e78cac7f0ae92a59ef320a2
jfcaballerop <jfcaballerop@gmail.com>
2023-09-15 18:28:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SseClient } from 'ngx-sse-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactiveDemoClient';
+
+  constructor(private sseClient: SseClient) {
+    // const headers = new HttpHeaders().set('Authorization', `Basic YWRtaW46YWRtaW4=`);
+
+    this.sseClient.stream('http://localhost:8080/weatherstream', { keepAlive: true, reconnectionDelay: 1_000, responseType: 'event' }, {}, 'GET').subscribe((event) => {
+      if (event.type === 'error') {
+        const errorEvent = event as ErrorEvent;
+        console.error(errorEvent.error, errorEvent.message);
+      } else {
+        const messageEvent = event as MessageEvent;
+        console.info(`SSE request with type "${messageEvent.type}" and data "${messageEvent.data}"`);
+      }
+    });
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+
 }
